d6723e18a090a3b9a3e30ea2dd8212cd5bf2d0ca
Andres Noboa V <56968012+aenoboa1@users.noreply.github.com>
2023-08-07 16:26:14 -0500

    mediapipe

--- read_video_test.py
+++ read_video_test.py
@@ -5,21 +5,22 @@ import cv2
 from deepface import DeepFace
 import mediapipe as mp
 
-video_data = st.file_uploader("Upload file", ['mp4','mov', 'avi'])
+video_data = st.file_uploader("Upload file", ['mp4', 'mov', 'avi'])
 
 temp_file_to_save = './temp_file_1.mp4'
-temp_file_result  = './temp_file_2.mp4'
+temp_file_result = './temp_file_2.mp4'
 
 backends = [
-  'opencv',
-  'ssd',
-  'dlib',
-  'mtcnn',
-  'retinaface',
-  'mediapipe',
-  'yolov8',
-  'yunet',
+    'opencv',
+    'ssd',
+    'dlib',
+    'mtcnn',
+    'retinaface',
+    'mediapipe',
+    'yolov8',
+    'yunet',
 ]
+
 
 # func to save BytesIO on a drive
 def write_bytesio_to_file(filename, bytesio):
@@ -32,66 +33,79 @@ def write_bytesio_to_file(filename, bytesio):
         # Copy the BytesIO stream to the output file
         outfile.write(bytesio.getbuffer())
 
+
 def detect_emotion(face_image):
-    resized_face_image = cv2.resize(face_image, (48, 48))
-    resized_face_image = np.expand_dims(resized_face_image, axis=0)
-    emotions = DeepFace.analyze(face_image, actions=['emotion'], enforce_detection=False)
+    # Ensure the face image is not empty
+
+    st.image(face_image)
+    image_batch = np.expand_dims(face_image, axis=0)
+    # Resize the face image to match the expected input shape of the emotion detection model
+    # Perform emotion detection using DeepFace
+    print(image_batch.shape)
+    emotions = DeepFace.analyze(
+        image_batch,
+        actions=['emotion'],
+        enforce_detection=False,
+        detector_backend=backends[4]
+        )
     emotion_label = emotions['dominant_emotion']
     return emotion_label
+
 
 def mediapipe_face_detection(image):
     mp_face_detection = mp.solutions.face_detection.FaceDetection(min_detection_confidence=0.5)
     results = mp_face_detection.process(image)
 
     if results.detections:
+        ih, iw, _ = image.shape
         for detection in results.detections:
             bboxC = detection.location_data.relative_bounding_box
-            ih, iw, _ = image.shape
             bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), int(bboxC.width * iw), int(bboxC.height * ih)
-
             x, y, w, h = bbox
+            # Ensure the bounding box coordinates are within image boundaries
+            x = max(0, x)
+            y = max(0, y)
+            w = min(iw - x, w)
+            h = min(ih - y, h)
             face_image = image[y:y + h, x:x + w]
-
             # Perform emotion detection using DeepFace
-            #emotion_label = detect_emotion(face_image)
-
-            # Draw the emotion label on the frame
-            #cv2.putText(image, emotion_label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
+            emotion_label = detect_emotion(face_image)
+            if emotion_label is not None:
+                # Draw the emotion label on the frame
+                cv2.putText(image, emotion_label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
     return image
+
+
 if video_data:
     # save uploaded video to disc
     write_bytesio_to_file(temp_file_to_save, video_data)
-
     # read it with cv2.VideoCapture(),
     # so now we can process it with OpenCV functions
     cap = cv2.VideoCapture(temp_file_to_save)
-
     # grab some parameters of video to use them for writing a new, processed video
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     frame_fps = cap.get(cv2.CAP_PROP_FPS)  ##<< No need for an int
     st.write(width, height, frame_fps)
-
     # specify a writer to write a processed video to a disk frame by frame
     fourcc_mp4 = cv2.VideoWriter_fourcc(*'mp4v')
-    out_mp4 = cv2.VideoWriter(temp_file_result, fourcc_mp4, frame_fps, (width, height),isColor = False)
+    out_mp4 = cv2.VideoWriter(temp_file_result, fourcc_mp4, frame_fps, (width, height), isColor=False)
 
     while True:
-        ret,frame = cap.read()
+        ret, frame = cap.read()
         if not ret:
             break
 
         emotion_dominant = mediapipe_face_detection(frame)
         # Draw the emotion label on the frame
-        #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) ##<< Generates a grayscale (thus only one 2d-array)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  ##<< Generates a grayscale (thus only one 2d-array)
 
         # Write the frame to the output video
-        out_mp4.write(frame)
+        out_mp4.write(gray)
 
         # Show the processed frame (you can comment this out for faster processing)
-        #cv2.imshow('Emotion Detection', frame)
+        # cv2.imshow('Emotion Detection', frame)
 
     cap.release()
     out_mp4.release()
@@ -111,9 +125,6 @@ if video_data:
     col2.header("After conversion to H264")
     col2.video(convertedVideo)
 
-
     ## Close video files
     out_mp4.release()
     cap.release()
-
-
