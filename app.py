9b422b659faf6d755da4aefac2e152020d90f6cf
Andres Noboa V <56968012+aenoboa1@users.noreply.github.com>
2023-08-12 05:49:04 +0000

    large-v2

--- app.py
+++ app.py
@@ -465,7 +465,7 @@ class EmotionRecognitionApp:
                         if whisper:
                             with st.spinner('Procesando Transcripción'):
                                 result = self.client.predict(
-                                    "large",
+                                    "large-v2",
                                     "Spanish",
                                     "",
                                     [f"./audio/{audio_filename}"],
